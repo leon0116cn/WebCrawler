73e8bb0d6363f292947399b550c09e7a73725969
Leon <leon0116cn@sina.com>
2019-05-30 11:27:09 +0800

    project v1.0

--- exchangequery.py
+++ exchangequery.py
@@ -1,15 +1,16 @@
 from datetime import datetime, timedelta
+import sys
 import requests
 from bs4 import BeautifulSoup
 
 
 class ExchangeRate:
 
-    def __init__(self):
+    def __init__(self, host):
         self._headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
         }
-        self._web_url = 'http://www.unionpayintl.com/cardholderServ/serviceCenter/rate?language=cn'
+        self._web_url = host + 'cardholderServ/serviceCenter/rate?language=cn'
         r = requests.get(self._web_url, headers=self._headers, timeout=5)
         r.raise_for_status()
         soup = BeautifulSoup(r.text, 'lxml')
@@ -17,7 +18,7 @@ class ExchangeRate:
         self._base_currency = (base['val'] for base in base_currency_list if base['val'] != '扣账币种')
         trans_currency_list = soup.find_all(id='transactionCurrency')
         self._transaction_currency = (trans['val'] for trans in trans_currency_list if trans['val'] != '交易币种')
-        self._search_url = 'http://www.unionpayintl.com' + soup.find(id='rateForm')['action']
+        self._search_url = host + soup.find(id='rateForm')['action']
 
     def query_exchange_rate(self, base_currency, transaction_currency, query_date):
         payload = {
@@ -42,10 +43,10 @@ class ExchangeRate:
 
 
 def main():
-     rate = ExchangeRate()
-     print(rate.query_exchange_rate('CNY', 'USD', (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')))
-     print(rate.query_exchange_rate('CNY', 'USD', (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')))
-     print(rate.query_exchange_rate('CNY', 'USD', (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d')))
+    rate = ExchangeRate(sys.argv[1])
+#    print(rate.query_exchange_rate('CNY', 'USD', datetime.now().strftime('%Y-%m-%d')))
+    print(rate.query_exchange_rate('CNY', 'USD', (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')))
+    print(rate.query_exchange_rate('CNY', 'USD', (datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d')))
 
 
 if __name__ == '__main__':
